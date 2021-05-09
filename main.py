7d5923183deeac64cf0689af62e7f1d0ed7542bf
lroberto68 <roberto_luongo@live.com>
2021-05-09 23:53:14 +0200

    Inserito metodo "CreaParteCognome"

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from codFiscale import CodFiscale
 
 def main():
-    print(__name__)
-    f=CodFiscale('Luongo','Roberto','M','1968-06-01','Pozzuoli')
-    print(f.stampaNome())
+    
+    f=CodFiscale("Luongo",'Roberto','M','1968-06-01','Pozzuoli')
+    
+    print(f.CreaParteCognome())
 
 if __name__=="__main__":
     main()
