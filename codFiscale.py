3f52de9624ced52656def9717ccfd6c1c07520ee
lroberto68 <roberto_luongo@live.com>
2021-05-10 00:09:23 +0200

    Formattato il codice

--- codFiscale.py
+++ codFiscale.py
@@ -1,42 +1,42 @@
 import datetime
 
+
 class CodFiscale:
 
-    def __init__(self,cognome, nome,sesso, data_nascita, luogo_nascita):
+    def __init__(self, cognome, nome, sesso, data_nascita, luogo_nascita):
         """Costruttore delle clase CodFiscale"""
 
-        self.__cognome=cognome.upper()
-        self.__nome=nome.upper()
-        self.__sesso=sesso
-        self.__data_nascita=data_nascita
-        self.__luogo_nascita=luogo_nascita
+        self.__cognome = cognome.upper()
+        self.__nome = nome.upper()
+        self.__sesso = sesso
+        self.__data_nascita = data_nascita
+        self.__luogo_nascita = luogo_nascita
 
-    def  __dividiConsVoc(self,cogNom):
+    def __dividiConsVoc(self, cogNom):
         """Metodo privato per separare consonanti e vocali in Cognoeme / Nome"""
 
-        VOCALI='AEIOU'
-        parteV=''
-        parteC=''
-        cogNom=cogNom.replace(" ","")
-        cogNom=cogNom.replace("'","")
-        
+        VOCALI = 'AEIOU'
+        parteV = ''
+        parteC = ''
+        cogNom = cogNom.replace(" ", "")
+        cogNom = cogNom.replace("'", "")
+
         for c in cogNom:
             if c not in VOCALI:
-                parteC+=c
+                parteC += c
             else:
-                parteV+=c
-        return parteC,parteV
+                parteV += c
+        return parteC, parteV
 
-    def __restaParte(self,consonanti, vocali):
+    def __restaParte(self, consonanti, vocali):
         """Metodo privato per unire Consonanti +Vocali"""
-        
-        parte=consonanti+vocali
-        parte=(parte.ljust(3,'X'))[0:3]
+
+        parte = consonanti + vocali
+        parte = (parte.ljust(3, 'X'))[0:3]
         return parte
 
+    def creaParteCognome(self):
+        """Metodo publico che restituisce Parte Cognome"""
 
-    def CreaParteCognome(self):
-        """Metodo publico che restituisce Parte Cognome"""      
-        parteC, parteV=self.__dividiConsVoc(self.__cognome)
-        return self.__restaParte(parteC,parteV)
-
+        parteC, parteV = self.__dividiConsVoc(self.__cognome)
+        return self.__restaParte(parteC, parteV)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from codFiscale import CodFiscale
 
-def main():
-    
-    f=CodFiscale("Luongo",'Roberto','M','1968-06-01','Pozzuoli')
-    
-    print(f.CreaParteCognome())
 
-if __name__=="__main__":
+def main():
+    f = CodFiscale("Luongo", 'Roberto', 'M', '1968-06-01', 'Pozzuoli')
+
+    print(f.creaParteCognome())
+
+
+if __name__ == "__main__":
     main()
