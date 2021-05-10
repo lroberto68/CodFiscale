d9752addfc2b1ee023595b4cb4a8d1abb4baf00c
lroberto68 <roberto_luongo@live.com>
2021-05-11 01:44:12 +0200

    Aggiunto metdo creaParteData

--- codFiscale.py
+++ codFiscale.py
@@ -1,19 +1,19 @@
-import datetime
+from datetime import date as dt
 
 
 class CodFiscale:
 
-    def __init__(self, cognome, nome, sesso, data_nascita, luogo_nascita):
+    def __init__(self, cognome, nome, sesso, dataNascita, luogoNascita):
         """Costruttore delle clase CodFiscale"""
 
         self.__cognome = cognome.upper()
         self.__nome = nome.upper()
         self.__sesso = sesso
-        self.__data_nascita = data_nascita
-        self.__luogo_nascita = luogo_nascita
+        self.__dataNascita = dataNascita
+        self.__luogoNascita = luogoNascita
 
     def __dividiConsVoc(self, cogNom):
-        """Metodo privato per separare consonanti e vocali in Cognoeme / Nome"""
+        """Metodo private per separare consonanti e vocali in Cognoeme / Nome"""
 
         VOCALI = 'AEIOU'
         parteV = ''
@@ -29,20 +29,20 @@ class CodFiscale:
         return parteC, parteV
 
     def __restaParte(self, consonanti, vocali):
-        """Metodo privato per unire Consonanti +Vocali"""
+        """Metodo private per unire Consonanti +Vocali"""
 
         parte = consonanti + vocali
         parte = (parte.ljust(3, 'X'))[0:3]
         return parte
 
     def creaParteCognome(self):
-        """Metodo publico che restituisce Parte Cognome"""
+        """Metodo public che restituisce Parte Cognome"""
 
         parteC, parteV = self.__dividiConsVoc(self.__cognome)
         return self.__restaParte(parteC, parteV)
 
     def creaParteNome(self):
-        """Metodo publico che restituisce Parte Nome"""
+        """Metodo public che restituisce Parte Nome"""
 
         parteC, parteV=self.__dividiConsVoc(self.__nome)
         l= len(parteC)
@@ -52,3 +52,18 @@ class CodFiscale:
              return parteC
 
         return self.__restaParte(parteC, parteV)
+
+    def creaParteData(self):
+        """Metodo public che restituisce Parte Data"""
+
+        tbMese={'Jan':'A','Feb':'B','Mar':'C','Apr':'D','May':'E','Jun':'H','Jul':'L','Aug':'M','Sep':'P','Oct':'R','Nov':'S','Dec':'T'}
+        
+        if self.__sesso=='F':
+            gg=40
+        elif self.__sesso=='M':
+            gg=0
+        
+        giorno=int(self.__dataNascita.strftime('%d'))+gg
+        parteData=self.__dataNascita.strftime('%y')+tbMese[self.__dataNascita.strftime('%b')]+ str(giorno).rjust(2,'0')
+        
+        return parteData
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from codFiscale import CodFiscale
-
+from datetime import date as dt
 
 def main():
-    f = CodFiscale("Luongo", 'Roberto', 'M', '1968-06-01', 'Pozzuoli')
+    f = CodFiscale("Luongo", 'Roberta', 'M', dt.fromisoformat('1968-06-01'), 'Pozzuoli')
 
-    print(f.creaParteCognome()+f.creaParteNome())
+    print(f.creaParteCognome()+f.creaParteNome()+f.creaParteData())
 
 
 if __name__ == "__main__":
